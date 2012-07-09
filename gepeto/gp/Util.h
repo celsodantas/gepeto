cc1840fbe1313782e6e31a339575aff315c5ef7d
Celso Dantas <celsodantas@gmail.com>
2012-07-09 11:16:41 -0300

    adding Util class OS dependant (get localtime)

--- gepeto/gp/Util.h
+++ gepeto/gp/Util.h
@@ -9,6 +9,23 @@
 #ifndef gepeto_Util_h
 #define gepeto_Util_h
 
+#ifdef DARWIN
+    #include "sys/macos/UtilAdapter.h"
+#else  // WINDOWS_OS
+    // TODO :: implement OS dependant Util
+    #include "sys/macos/UtilAdapter.h"
+#endif
 
+class Util
+{
+    UtilAdapter adapter;
+
+public:
+    
+    static inline double elapsedTimeSecs()
+    {
+        return UtilAdapter::elapsedTimeSecs();
+    }
+};
 
 #endif
--- gepeto/gp/sys/macos/UtilAdapter.h
+++ gepeto/gp/sys/macos/UtilAdapter.h
@@ -6,9 +6,36 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#ifndef gepeto_Util_h
-#define gepeto_Util_h
+#ifndef gepeto_UtilAdapter_h
+#define gepeto_UtilAdapter_h
 
+#include <sys/time.h>
 
+///////////////////
+//
+// This it not a full adapter but it is working like one. =D
+//
+///////////////////
+
+class UtilAdapter
+{
+public:
+    
+    static inline double elapsedTimeSecs()
+    {
+        static timeval now, prev;
+        static long elapsed, seconds, useconds;    
+        
+        prev = now;
+        gettimeofday(&now, NULL);
+        
+        seconds  = now.tv_sec  - prev.tv_sec;
+        useconds = now.tv_usec - prev.tv_usec;
+        
+        elapsed = ((seconds) * 1000 + useconds/1000.0);
+        
+        return elapsed/1000.0;
+    }
+};
 
 #endif

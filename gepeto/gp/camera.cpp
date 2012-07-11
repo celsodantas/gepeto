4be0882b66aa6b455f8edb9bcf3911d19c52df8a
Celso Dantas <celsodantas@gmail.com>
2012-07-10 22:22:41 -0300

    implementing camera class

--- gepeto/gp/camera.cpp
+++ gepeto/gp/camera.cpp
@@ -7,3 +7,49 @@
 //
 
 #include <iostream>
+#include "Camera.h"
+
+void Camera::resize()
+{
+    double fov   = 45, 
+    aspect       = 1.333333, 
+    farClipDist  = 500, 
+    nearClipDist = 0.1;
+    
+    glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+    
+    gluPerspective(fov, 
+				   aspect, 
+				   nearClipDist, 
+				   farClipDist);
+}
+
+void Camera::lookAt(double x, double y, double z)
+{
+    _lookX = x;
+    _lookY = y;
+    _lookZ = z;
+}
+
+void Camera::setPosition(double x, double y, double z)
+{
+    _posX = x;
+    _posY = y;
+    _posZ = z;
+}
+
+void Camera::addPosX(double v)
+{
+    _posX += v;
+}
+
+void Camera::addPosY(double v)
+{
+    _posY += v;
+}
+
+void Camera::addPosZ(double v)
+{
+    _posZ += v;
+}
--- gepeto/gp/camera.h
+++ gepeto/gp/camera.h
@@ -9,6 +9,49 @@
 #ifndef gepeto_camera_h
 #define gepeto_camera_h
 
+#include "glfw.h"
 
+class Camera
+{
+public:
+    Camera() {
+        _upX = 0; 
+        _upY = 1; 
+        _upZ = 0;
+        
+    }
+    
+    void addPosX(double v);
+    void addPosY(double v);
+    void addPosZ(double v);
+    
+    void lookAt(double x, double y, double z);
+    void setPosition(double x, double y, double z);
+    void resize();
+    
+    inline void update() {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity(); 
+        
+        gluLookAt(_posX,  _posY,  _posZ, 
+                  _lookX, _lookY, _lookZ, 
+                  _upX,   _upY,   _upZ);
+    }
+    
+private:
+    float _posX,
+          _posY,
+          _posZ;
+    
+    float _lookX, 
+          _lookY, 
+          _lookZ;
+    
+    float _upX, 
+          _upY, 
+          _upZ;
+    
+    int _matrix_mode;
+};
 
 #endif
